241dcfc63a13c97d271c313a682f422adac50e18
Amit <amitoz.deol@eawelectro.com>
2017-03-15 10:51:28 -0400

    navigation view is working!!

--- app/src/main/java/com/eaw/MainActivity.java
+++ app/src/main/java/com/eaw/MainActivity.java
@@ -1,8 +1,12 @@
 package com.eaw;
 
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,15 +16,33 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.JavascriptInterface;
+import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.CompoundButton;
 import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
-    String url = "http://10.0.2.2/2020_web/public";
+    final String url = "http://10.0.2.2/2020_web/public";
+    ArrayList<String> navbarURL = new ArrayList<String>();
+    ArrayList<String> navbarNAME = new ArrayList<String>();
+    ArrayList<String> navbar_account_URL = new ArrayList<String>();
+    ArrayList<String> navbar_account_NAME = new ArrayList<String>();
+    String username;
+    Boolean toggleON;
+    Menu menu;
+    NavigationView navigationView;
     ProgressBar spinner;
     WebView webView;
+    ToggleButton accountView;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,7 +52,11 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         spinner = (ProgressBar) findViewById(R.id.loadingPanel);
         webView= (WebView) findViewById(R.id.webview);
         webView.setWebViewClient(new MyBrowser());
-
+        webView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
+        webView.getSettings().setJavaScriptEnabled(true);
+        //turn this on if wanted to see the website on zoomed out level
+        //webView.getSettings().setUseWideViewPort(true);
+        webView.getSettings().setLoadWithOverviewMode(true);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -38,28 +64,59 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-        loadURL(url);
-    }
 
-    private void loadURL(String url) {
-        spinner.setProgress(0);
-        spinner.setVisibility(View.VISIBLE);
-        webView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.getSettings().setUseWideViewPort(true);
-        webView.getSettings().setLoadWithOverviewMode(true);
-        webView.setWebChromeClient(new WebChromeClient(){
-            public void onProgressChanged(WebView view, int progress) {
-                spinner.setProgress(progress);
-                if(progress >= 80) {
-                    spinner.setVisibility(View.GONE);
+        View hView =  navigationView.getHeaderView(0);
+        textView= (TextView) hView.findViewById(R.id.nav_head_username);
+
+        accountView = (ToggleButton)hView.findViewById(R.id.account_view_icon_button);
+        accountView.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                menu = navigationView.getMenu();
+                menu.clear();
+                toggleON = isChecked;
+                if (isChecked){
+                    for (int i=0;i<navbar_account_NAME.size();i++) {
+                        menu.add(R.id.nav_group,i,Menu.NONE,navbar_account_NAME.get(i));
+                    }
+                }else{
+                    for (int i=0;i<navbarNAME.size();i++) {
+                        menu.add(R.id.nav_group,i,Menu.NONE,navbarNAME.get(i));
+                    }
                 }
             }
         });
 
+        loadURL(url);
+    }
 
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // Check if the key event was the Back button and if there's history
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()) {
+            webView.goBack();
+            return true;
+        }
+        // If it wasn't the Back key or there's no web page history, bubble up to the default
+        // system behavior (probably exit the activity)
+        return super.onKeyDown(keyCode, event);
+    }
+
+    private void loadURL(final String url) {
+        spinner.setProgress(0);
+        spinner.setVisibility(View.VISIBLE);
+        webView.setWebChromeClient(new WebChromeClient() {
+            public void onProgressChanged(WebView view, int progress) {
+                spinner.setProgress(progress);
+                if (progress >= 80) {
+                    spinner.setVisibility(View.GONE);
+                }
+            }
+        });
+        //get data from javascript function
+        webView.addJavascriptInterface(new WebAppInterface(), "Android");
         webView.loadUrl(url);
     }
 
@@ -75,10 +132,12 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+        // Inflate the menu; this adds items to the action bar if it is present. Currently Settings
         getMenuInflater().inflate(R.menu.main, menu);
+        this.menu = menu;
         return true;
     }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -86,7 +145,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
@@ -100,48 +158,58 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
-
-        if (id == R.id.nav_home) {
-            // Handle the home action
-            webView.getSettings().setDomStorageEnabled(true);
-            webView.setWebViewClient(new WebViewClient() {
-                @Override
-                public void onPageFinished(WebView view, String url) {
-                    webView.loadUrl("javascript:(function() { " +
-                            "document.getElementById('navbar')[0].href; " +
-                            "document.getElementById('inputID').value=''; " +
-                            "})()");
+        if (toggleON){
+            for (int i=0;i<navbar_account_URL.size();i++){
+                if (id==i){
+                    loadURL(navbar_account_URL.get(i));
                 }
-            });
-            loadURL(url+"/home");
-
-        } else if (id == R.id.nav_product) {
-            loadURL(url+"/products");
-
-        } else if (id == R.id.nav_services) {
-            loadURL(url+"/services");
-
-        } else if (id == R.id.nav_about) {
-            loadURL(url+"/aboutus");
-
-        } else if (id == R.id.nav_support) {
-            loadURL(url+"/support");
-
-        } else if (id == R.id.nav_login) {
-            loadURL(url+"/login");
-
+            }
+        }else{
+            for (int i=0;i<navbarURL.size();i++){
+                if (id==i){
+                    loadURL(navbarURL.get(i));
+                }
+            }
         }
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
+        accountView.setChecked(false);
         return true;
     }
 
     private class MyBrowser extends WebViewClient {
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            view.loadUrl(url);
-            return true;
+
+        public void onPageFinished(WebView view, String url) {
+            // do your stuff here
+            menu = navigationView.getMenu();
+            menu.clear();
+            for (int i=0;i<navbarNAME.size();i++) {
+                menu.add(R.id.nav_group,i,Menu.NONE,navbarNAME.get(i));
+            }
+            textView.setText(username);
+        }
+    }
+
+    private class WebAppInterface {
+        @JavascriptInterface
+        public void sendData(String[] data, String[] data_dropdown) {
+            navbarNAME.clear();
+            navbarURL.clear();
+            navbar_account_NAME.clear();
+            navbar_account_URL.clear();
+            username = data[0];
+            System.out.println(data[0]);
+            //Get the string value to process
+            for (int i=1; i<data.length; i++){
+                navbarURL.add(data[i]);
+                i++;
+                navbarNAME.add(data[i]);
+            }
+            for (int i=0; i<data_dropdown.length; i++){
+                navbar_account_URL.add(data_dropdown[i]);
+                i++;
+                navbar_account_NAME.add(data_dropdown[i]);
+            }
         }
     }
 }
